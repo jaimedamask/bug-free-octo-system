fbf272c4114f55a63c00d70e6eed129c81c0f69f
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-27 18:50:05 -0500

    Feat: Add border to grid-container and center on page

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const gridContainer = document.getElementById('grid-container');
-
+let containerWidth = gridContainer.offsetWidth;
 let input = 16;
 let totalSquares = Math.pow(input, 2);
 
 
 function makeGrid() {
+    const sideLength = Math.round(containerWidth / input);
+    const sideLengthStr = sideLength + 'px';
+
     for (let i = 0; i < totalSquares; i++) {
         const newSquare = document.createElement('div');
         newSquare.classList.add('square');
         gridContainer.appendChild(newSquare);
+        newSquare.style.width = sideLengthStr;
+        newSquare.style.height = sideLengthStr;
     }
 }
 
