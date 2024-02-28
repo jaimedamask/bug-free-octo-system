bf6c1375d8322519c14bb03da6a625603c126288
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-27 20:31:44 -0500

    Fix: Add recalculation of gridContainer size to makeGrid so squares do not overflow

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const gridContainer = document.getElementById('grid-container');
 let containerWidth = gridContainer.clientWidth;
-let input = 16;
+let input = Number(prompt('Choose a number between 1 and 100'));
 let totalSquares = Math.pow(input, 2);
 
 
 function makeGrid() {
     const sideLength = Math.round(containerWidth / input);
     const sideLengthStr = sideLength + 'px';
+    let fullWidth;
 
     for (let i = 0; i < totalSquares; i++) {
         const newSquare = document.createElement('div');
@@ -14,7 +15,12 @@ function makeGrid() {
         gridContainer.appendChild(newSquare);
         newSquare.style.width = sideLengthStr;
         newSquare.style.height = sideLengthStr;
+        fullWidth += sideLength;
     }
+
+    let fullWidthStr = fullWidth + 'px';
+    gridContainer.style.width = fullWidthStr;
+    gridContainer.style.height = fullWidthStr;
 }
 
 makeGrid();
