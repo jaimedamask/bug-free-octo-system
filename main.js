ff03dc151bba379198c937fd9a7c85db6bf7969a
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-02-27 20:59:47 -0500

    Fix: Correct recalculation of gridContainer size so certain inputs do not cause overflow

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const gridContainer = document.getElementById('grid-container');
 let containerWidth = gridContainer.clientWidth;
 let input = Number(prompt('Choose a number between 1 and 100'));
 let totalSquares = Math.pow(input, 2);
-
+console.log(containerWidth);
 
 function makeGrid() {
-    const sideLength = Math.round(containerWidth / input);
+    const sideLength = containerWidth / input;
     const sideLengthStr = sideLength + 'px';
-    let fullWidth;
 
     for (let i = 0; i < totalSquares; i++) {
         const newSquare = document.createElement('div');
@@ -15,12 +14,13 @@ function makeGrid() {
         gridContainer.appendChild(newSquare);
         newSquare.style.width = sideLengthStr;
         newSquare.style.height = sideLengthStr;
-        fullWidth += sideLength;
     }
 
+    let fullWidth = sideLength * input;
     let fullWidthStr = fullWidth + 'px';
     gridContainer.style.width = fullWidthStr;
     gridContainer.style.height = fullWidthStr;
+    console.log(fullWidthStr);
 }
 
 makeGrid();
